57e0e4fc92c26a86fda64ed9f6ab8110df450300
Kinue72 <kinue137@gmail.com>
2024-11-02 11:37:51 +0700

    mediatek: init vendor driver backport

--- target/linux/mediatek/modules.mk
+++ target/linux/mediatek/modules.mk
@@ -37,3 +37,22 @@ define KernelPackage/iio-mt6577-auxadc
   $(call AddDepends/iio)
 endef
 $(eval $(call KernelPackage,iio-mt6577-auxadc))
+
+define KernelPackage/mediatek_hnat
+  SUBMENU:=Network Devices
+  TITLE:=Mediatek HNAT module
+  DEPENDS:=@TARGET_mediatek +kmod-nf-conntrack
+  AUTOLOAD:=$(call AutoLoad,20,mtkhnat)
+  KCONFIG:= \
+	CONFIG_BRIDGE_NETFILTER=y \
+	CONFIG_NETFILTER_FAMILY_BRIDGE=y \
+	CONFIG_NET_MEDIATEK_HNAT=y
+  FILES:= \
+        $(LINUX_DIR)/drivers/net/ethernet/mediateksdk/mtk_hnat/mtkhnat.ko
+endef
+
+define KernelPackage/mediatek_hnat/description
+  Kernel modules for MediaTek HW NAT offloading
+endef
+
+$(eval $(call KernelPackage,mediatek_hnat))
